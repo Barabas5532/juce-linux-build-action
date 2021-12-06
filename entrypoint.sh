ff8de7d5e3e09d1ce25883afd1b5e3d52079d60c
Barabas Raffai <barabas.raffai@gmail.com>
2021-12-06 20:05:44 +0000

    Fix older cmake using incorrect build type

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printenv
-cmake -B build . && cmake --build build
+# cmake <3.22 does not use the CMAKE_BUILD_TYPE environment variable
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -B build . && cmake --build build
